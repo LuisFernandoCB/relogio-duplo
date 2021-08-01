2f577ff33366cc829aa170874ee5e16f0069c1c3
Shad Wolf <lfcb0820@gmail.com>
2021-08-01 13:53:40 -0300

    a

--- JS/codigo3.js
+++ JS/codigo3.js
@@ -6,6 +6,7 @@ var btn1 = document.querySelector("#tema1")
 var btn2 = document.querySelector("#tema2")
 var btn3 = document.querySelector("#tema3")
 var btn4 = document.querySelector("#tema4")
+var temaVez = Boolean
 
 
 
@@ -41,47 +42,58 @@ if (hora >= 6 && hora < 8) {
     btn4.style.background = "darkslateblue"
 }
 
-function clicou1() {
-    if (hora >= 6 && hora < 8) {
-        document.body.style.background = "dodgerblue"
-        document.body.style.color = "khaki"
-        imagem.innerHTML = '<img src="IMAGES/dia.png" alt="KKK" >'
-    } else {
-        document.body.style.background = "khaki"
-        document.body.style.color = "darkolivegreen"
-        imagem.innerHTML = '<img src="IMAGES/amanhecer.png" alt="KKK">'
-    }
+function saiu() {
+    temaVez = true
+}
 
-    if (hora >= 6 && hora < 8) {
-        btn1.style.background = "rgb(22, 115, 207)"
-        btn2.style.background = "darkmagenta"
-        btn3.style.background = "darkslateblue"
-        btn4.style.background = "midnightblue"
-    } else if (hora >= 8 && hora < 17) {
-        btn1.style.background = "rgb(202, 194, 117)"
-        btn2.style.background = "darkmagenta"
-        btn3.style.background = "darkslateblue"
-        btn4.style.background = "midnightblue"
-    } else if (hora >= 17 && hora < 18) {
-        btn1.style.background = "rgb(202, 194, 117)"
-        btn2.style.background = "dodgerblue"
-        btn3.style.background = "darkslateblue"
-        btn4.style.background = "midnightblue"
-    } else if (hora >= 18 && hora < 19) {
-        btn1.style.background = "rgb(202, 194, 117)"
-        btn2.style.background = "dodgerblue"
-        btn3.style.background = "darkmagenta"
-        btn4.style.background = "midnightblue"
-    } else if (hora >= 19) {
-        btn1.style.background = "rgb(202, 194, 117)"
-        btn2.style.background = "dodgerblue"
-        btn3.style.background = "darkmagenta"
-        btn4.style.background = "darkslateblue"
+function clicou1() {
+    if(temaVez == true) {
+        //muda pro tema da hora
+        if (hora >= 6 && hora < 8) {
+            document.body.style.background = "dodgerblue"
+            document.body.style.color = "khaki"
+            imagem.innerHTML = '<img src="IMAGES/dia.png" alt="KKK" >'
+        } else {
+            document.body.style.background = "khaki"
+            document.body.style.color = "darkolivegreen"
+            imagem.innerHTML = '<img src="IMAGES/amanhecer.png" alt="KKK">'
+        }
+    
+        if (hora >= 6 && hora < 8) {
+            btn1.style.background = "rgb(22, 115, 207)"
+            btn2.style.background = "darkmagenta"
+            btn3.style.background = "darkslateblue"
+            btn4.style.background = "midnightblue"
+        } else if (hora >= 8 && hora < 17) {
+            btn1.style.background = "rgb(202, 194, 117)"
+            btn2.style.background = "darkmagenta"
+            btn3.style.background = "darkslateblue"
+            btn4.style.background = "midnightblue"
+        } else if (hora >= 17 && hora < 18) {
+            btn1.style.background = "rgb(202, 194, 117)"
+            btn2.style.background = "dodgerblue"
+            btn3.style.background = "darkslateblue"
+            btn4.style.background = "midnightblue"
+        } else if (hora >= 18 && hora < 19) {
+            btn1.style.background = "rgb(202, 194, 117)"
+            btn2.style.background = "dodgerblue"
+            btn3.style.background = "darkmagenta"
+            btn4.style.background = "midnightblue"
+        } else if (hora >= 19) {
+            btn1.style.background = "rgb(202, 194, 117)"
+            btn2.style.background = "dodgerblue"
+            btn3.style.background = "darkmagenta"
+            btn4.style.background = "darkslateblue"
+        } else {
+            btn1.style.background = "rgb(202, 194, 117)"
+            btn2.style.background = "dodgerblue"
+            btn3.style.background = "darkmagenta"
+            btn4.style.background = "darkslateblue"
+        }
+        temaVez = false
     } else {
-        btn1.style.background = "rgb(202, 194, 117)"
-        btn2.style.background = "dodgerblue"
-        btn3.style.background = "darkmagenta"
-        btn4.style.background = "darkslateblue"
+        //fica normal
+        
     }
 }
 
